7ace0abc735fa79c80c43eaa7b07632d6571982e
Nicole Venner <nvenner@protonmail.ch>
2024-04-30 09:47:27 -0600

    refactor stuff

---
+++ home/packages/server-essentials.nix
@@ -0,0 +1,37 @@
+{ inputs, lib, config, pkgs, ... }: {
+  home.packages = with pkgs; [
+    # Terminal Utils
+    tmux
+    lshw
+    age
+    pciutils
+    parted
+    exfat
+    btop
+    pv # pipe viewer??
+    zip
+    wget
+    lazygit
+    # Neofetch Clones
+    neofetch
+    # Crypto and Stuff
+    gnupg
+    git-crypt
+    # Server web browser
+    elinks
+    git
+    exfat
+    exfatprogs
+    dig
+    hugo
+    tailscale
+    trayscale
+    speedtest-cli
+    nushell
+    btop
+    keepassxc
+    # audio
+    pavucontrol
+    helvum
+  ];
+}
--- system/server-pkgs.nix
+++ system/server-pkgs.nix
@@ -1,38 +1,5 @@
 { inputs, lib, config, pkgs, ... }: {
-  services.prometheus.exporters.smartctl.enable = true;
   environment.systemPackages = with pkgs;[
-    # Terminal Utils
     tmux
-    lshw
-    age
-    pciutils
-    parted
-    exfat
-    btop
-    pv # pipe viewer??
-    zip
-    wget
-    lazygit
-    # Neofetch Clones
-    neofetch
-    # Crypto and Stuff
-    gnupg
-    git-crypt
-    # Server web browser
-    elinks
-    git
-    exfat
-    exfatprogs
-    dig
-    hugo
-    tailscale
-    trayscale
-    speedtest-cli
-    nushell
-    btop
-    keepassxc
-    # audio
-    pavucontrol
-    helvum
   ];
 }
