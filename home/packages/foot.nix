1e8048df7fc2147a87ff47016125b95980ca9f56
Nicole Venner <nvenner@protonmail.ch>
2024-05-12 19:44:56 -0600

    finally committing to monoid

--- home/packages/foot.nix
+++ home/packages/foot.nix
@@ -1,7 +1,7 @@
 { inputs, lib, config, pkgs, ... }:  {
   programs.foot  = {
     enable = true;
-    settings.main.font = "monocraft:size=15";
+    settings.main.font = "Monoid Nerd Font:size=15";
     settings.colors.alpha=0.7;
     settings.colors.background="000000";
   };
