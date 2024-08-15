5ded61c2e797db1ee5bd8f5019c06387c603aba8
Nicole Venner <nvenner@protonmail.ch>
2024-08-15 10:11:47 -0600

    add gpt neovim plugin

--- home/packages/config/nvim/lua/plugins/example.lua
+++ home/packages/config/nvim/lua/plugins/example.lua
@@ -6,6 +6,17 @@
 -- * override the configuration of LazyVim plugins
 return {
   {
+      "robitx/gp.nvim",
+      config = function()
+          local conf = {
+              -- For customization, refer to Install > Configuration in the Documentation/Readme
+          }
+          require("gp").setup(conf)
+
+          -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
+      end,
+  },
+  {
     "rcarriga/nvim-notify",
     keys = {
       {
