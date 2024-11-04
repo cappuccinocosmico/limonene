d381b6c2ff8110e8448f13b7294fb63a3b59658a
Nicole Venner <nvenner@protonmail.ch>
2024-11-03 20:26:40 -0700

    fix micro

--- home/packages/nvim.nix
+++ home/packages/nvim.nix
@@ -3,18 +3,21 @@
     ripgrep
     ripgrep-all
   ];
-    programs.neovim = {
-      enable = true;
-        extraPackages = with pkgs; [
-            parinfer-rust
-            tree-sitter # to build grammars from source
-            libgcc
-        ];
-      viAlias = true;
-      vimAlias = true;
-    };
-    xdg.configFile."nvim" = {
-      recursive = true;
-      source = config/nvim;
-    };
+  programs.neovim = {
+    enable = true;
+      extraPackages = with pkgs; [
+          parinfer-rust
+          tree-sitter # to build grammars from source
+          libgcc
+      ];
+    viAlias = true;
+    vimAlias = true;
+  };
+  xdg.configFile."nvim" = {
+    recursive = true;
+    source = config/nvim;
+  };
+  programs.micro = {
+    enable = true;
+  }
 }
