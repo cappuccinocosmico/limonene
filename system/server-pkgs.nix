f4d9e45d4f51d4468ceca5aea00da24f10ce80e8
Nicole Venner <nvenner@protonmail.ch>
2025-03-23 16:45:50 -0600

    add stuff for nixos specific builds

--- system/server-pkgs.nix
+++ system/server-pkgs.nix
@@ -1,5 +1,9 @@
 { inputs, lib, config, pkgs, ... }: {
   environment.systemPackages = with pkgs;[
     tmux
+    bat # cat but actually adds formatting stuff 
+    dua # disk usage but actually useful
+    btop
+    zoxide
   ];
 }
