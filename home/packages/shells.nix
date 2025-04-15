7b82bd25367c1d7090ae03f56fbf9ab4441dfc7f
Nicole Venner <nvenner@protonmail.ch>
2025-04-14 22:29:03 -0600

    add function to fish?

--- home/packages/shells.nix
+++ home/packages/shells.nix
@@ -27,6 +27,17 @@ $env.PATH | split row (char esep)
   };
   programs.fish  = {
     enable = true;
+    functions = {
+      envsource = ''
+        function envsource
+          for line in (cat $argv | grep -v '^#')
+            set item (string split -m 1 '=' $line)
+            set -gx $item[1] $item[2]
+            echo "Exported key $item[1]"
+          end
+        end
+      '';
+    };
   };
   programs.oh-my-posh= {
     enable = false;
