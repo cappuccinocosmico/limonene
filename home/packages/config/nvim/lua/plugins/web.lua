2581cf4d8248780654e112e22ae51bc52eab8c37
Nicole Venner <nvenner@protonmail.ch>
2025-04-18 17:23:50 -0600

    remove leetcode

--- home/packages/config/nvim/lua/plugins/web.lua
+++ home/packages/config/nvim/lua/plugins/web.lua
@@ -1,36 +1,3 @@
 return {
-  {
-      "kawre/leetcode.nvim",
-      build = ":TSUpdate html",
-      dependencies = {
-          "nvim-telescope/telescope.nvim",
-          "nvim-lua/plenary.nvim", -- required by telescope
-          "MunifTanjim/nui.nvim",
-
-          -- optional
-          "nvim-treesitter/nvim-treesitter",
-          "rcarriga/nvim-notify",
-          "nvim-tree/nvim-web-devicons",
-      },
-      opts = {
-          -- configuration goes here
-      },
-  },
-  {
-    "kawre/leetcode.nvim",
-    build = ":TSUpdate html",
-    dependencies = {
-        "nvim-telescope/telescope.nvim",
-        "nvim-lua/plenary.nvim", -- required by telescope
-        "MunifTanjim/nui.nvim",
-
-        -- optional
-        "nvim-treesitter/nvim-treesitter",
-        "rcarriga/nvim-notify",
-        "nvim-tree/nvim-web-devicons",
-    },
-    opts = {
-        -- configuration goes here
-    },
-  }
+  { 'glacambre/firenvim', build = ":call firenvim#install(0)" }
 }
