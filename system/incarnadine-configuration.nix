05ccc514a9ec2107aaff611066f99ec27d4cb168
Nicole Venner <nvenner@protonmail.ch>
2025-08-31 17:35:35 -0600

    add flatpak

--- system/incarnadine-configuration.nix
+++ system/incarnadine-configuration.nix
@@ -10,7 +10,7 @@
       ./incarnadine-hardware.nix
     ];
 
-
+services.flatpak.enable = true;
 programs.steam = {
   enable = true;
   remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
