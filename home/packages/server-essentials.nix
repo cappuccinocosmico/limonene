2f4fb32f822d5dedfeb28af7a3e7c7344dfc34ce
Nicole Venner <nvenner@protonmail.ch>
2025-10-04 04:57:19 -0600

    include network scanning utilities

--- home/packages/server-essentials.nix
+++ home/packages/server-essentials.nix
@@ -1,6 +1,7 @@
 { inputs, lib, config, pkgs, ... }: {
   home.packages = with pkgs; [
-    # sqlite-interactive
+    nmap 
+    arp-scan-rs
     sqlite
     # better cli tools 
     dua # disk usage but actually useful
