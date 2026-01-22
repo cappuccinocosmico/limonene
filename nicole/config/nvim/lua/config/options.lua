2679c70f26495b122c183c455b23c80f47953783
Nicole Venner <nvenner@protonmail.ch>
2026-01-22 15:41:39 -0700

    delete lua config for old repo

--- nicole/config/nvim/lua/config/options.lua
+++
@@ -1,8 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-local opt = vim.opt
-opt.wrap = true
--- Set to "basedpyright" to use basedpyright instead of pyright.
-vim.g.lazyvim_python_lsp = "pyright"
-vim.g.snacks_animate = false
