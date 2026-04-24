bed2355fd6fb51fc645ff024c509ea5fd75a9fb7
Nicole Venner <nvenner@protonmail.ch>
2026-04-24 15:51:28 -0600

    add cryptpad

--- modules/features/media-server.nix
+++ modules/features/media-server.nix
@@ -1,5 +1,9 @@
-{ inputs, lib, ... }: {
-  flake.modules.nixos.mediaServer = { pkgs, ... }: {
+{
+  inputs,
+  lib,
+  ...
+}: {
+  flake.modules.nixos.mediaServer = {pkgs, ...}: {
     virtualisation.docker.enable = lib.mkForce false;
 
     services.jellyfin = {
@@ -7,13 +11,63 @@
       openFirewall = true;
       user = "jellyfin";
     };
+    services.cryptpad = {
+      enable = true;
+      openPorts = true;
 
+      settings = {
+        httpPort = 9000;
+        httpAddress = "0.0.0.0";
+        httpUnsafeOrigin = "http://localhost:${toString config.services.cryptpad.settings.httpPort}";
+        httpSafeOrigin = "http://localhost:${toString config.services.cryptpad.settings.httpPort}";
+      };
+    };
+    vpnNamespaces.wg = {
+      enable = true;
+      wireguardConfigFile = "/etc/nixos/secrets/privado.den-017.conf";
+      accessibleFrom = ["127.0.0.1" "192.168.0.0/16"];
+      portMappings = [
+        {
+          from = 9091;
+          to = 9091;
+        }
+      ];
+      openVPNPorts = [
+        {
+          port = 51413;
+          protocol = "both";
+        }
+      ];
+    };
+
+    systemd.services.transmission.vpnConfinement = {
+      enable = true;
+      vpnNamespace = "wg";
+    };
+
+    services.transmission = {
+      enable = true;
+      package = inputs.nixpkgs.legacyPackages.x86_64-linux.transmission_4;
+      openRPCPort = true;
+      openPeerPorts = true;
+      user = "jellyfin";
+      group = "jellyfin";
+      settings = {
+        rpc-bind-address = "192.168.15.1";
+        rpc-whitelist = "192.168.15.5,127.0.0.1,192.168.0.*,192.168.68.*";
+        peer-port = 51413;
+        download-dir = "/media/entertain";
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = [111 2049 4000 4001 4002 9091 20048 51413 9000];
+    networking.firewall.allowedUDPPorts = [111 2049 4000 4001 4002 9091 20048 51413 9000];
     users.groups.jellyfin = {};
     users.users.jellyfin = {
       isSystemUser = true;
       description = "Jellyfin System User";
       shell = pkgs.bashInteractive;
-      extraGroups = [ "render" "video" ];
+      extraGroups = ["render" "video"];
     };
 
     services.gvfs.enable = true;
@@ -23,12 +77,12 @@
 
     virtualisation.containers = {
       enable = true;
-      registries.search = [ "docker.io" ];
+      registries.search = ["docker.io"];
       policy = {
-        default = [ { type = "insecureAcceptAnything"; } ];
+        default = [{type = "insecureAcceptAnything";}];
         transports = {
           docker-daemon = {
-            "" = [ { type = "insecureAcceptAnything"; } ];
+            "" = [{type = "insecureAcceptAnything";}];
           };
         };
       };
--- modules/systems/amon-sul.nix
+++ modules/systems/amon-sul.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }: {
+{inputs, ...}: {
   flake.nixosConfigurations.amon-sul = inputs.nixpkgs.lib.nixosSystem {
     system = "x86_64-linux";
     modules = [
@@ -14,17 +14,20 @@
       inputs.self.modules.nixos.mediaServer
       inputs.vpn-confinement.nixosModules.default
       ../../hardware/amon-sul.nix
-      ({ pkgs, ... }: {
+      ({pkgs, ...}: {
         limonene.machineType = "server";
 
         networking.hostName = "amon-sul";
 
         networking.interfaces.enp11s0.useDHCP = false;
         networking.interfaces.enp11s0.ipv4.addresses = [
-          { address = "192.168.0.7"; prefixLength = 24; }
+          {
+            address = "192.168.0.7";
+            prefixLength = 24;
+          }
         ];
         networking.defaultGateway = "192.168.0.1";
-        networking.nameservers = [ "8.8.8.8" "1.1.1.1" ];
+        networking.nameservers = ["8.8.8.8" "1.1.1.1"];
 
         boot.loader.systemd-boot.enable = true;
         boot.loader.efi.canTouchEfiVariables = true;
@@ -44,64 +47,33 @@
 
         users.groups.brad = {};
         users.users.brad.group = "brad";
-        users.users.nicole.extraGroups = [ "jellyfin" ];
+        users.users.nicole.extraGroups = ["jellyfin"];
         users.users.nicole.openssh.authorizedKeys.keys = [
           "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKdPzSlJ3TCzPy7R2s2OOBJbBb+U5NY8dwMlGH9wm4Ot nicole@apiarist"
         ];
-        users.users.brad.extraGroups = [ "jellyfin" ];
+        users.users.brad.extraGroups = ["jellyfin"];
 
         fileSystems."/backup" = {
           device = "/dev/disk/by-uuid/7b72c3a2-9a4b-4f43-b787-c179ec71847e";
           fsType = "btrfs";
-          options = [ "users" "nofail" "x-gvfs-show" ];
+          options = ["users" "nofail" "x-gvfs-show"];
         };
 
         fileSystems."/media" = {
           device = "/dev/disk/by-uuid/5424a16e-700b-4620-b7f9-713a1619eb88";
           fsType = "btrfs";
-          options = [ "users" "nofail" "x-gvfs-show" ];
+          options = ["users" "nofail" "x-gvfs-show"];
         };
 
         fileSystems."/export/media" = {
           device = "/media";
           fsType = "none";
-          options = [ "bind" ];
+          options = ["bind"];
         };
 
         services.nfs.server.exports = ''
           /media   192.168.0.0/16(rw,nohide,insecure,no_subtree_check)
         '';
-
-        vpnNamespaces.wg = {
-          enable = true;
-          wireguardConfigFile = "/etc/nixos/secrets/privado.den-017.conf";
-          accessibleFrom = [ "127.0.0.1" "192.168.0.0/16" ];
-          portMappings = [{ from = 9091; to = 9091; }];
-          openVPNPorts = [{ port = 51413; protocol = "both"; }];
-        };
-
-        systemd.services.transmission.vpnConfinement = {
-          enable = true;
-          vpnNamespace = "wg";
-        };
-
-        services.transmission = {
-          enable = true;
-          package = inputs.nixpkgs.legacyPackages.x86_64-linux.transmission_4;
-          openRPCPort = true;
-          openPeerPorts = true;
-          user = "jellyfin";
-          group = "jellyfin";
-          settings = {
-            rpc-bind-address = "192.168.15.1";
-            rpc-whitelist = "192.168.15.5,127.0.0.1,192.168.0.*,192.168.68.*";
-            peer-port = 51413;
-            download-dir = "/media/entertain";
-          };
-        };
-
-        networking.firewall.allowedTCPPorts = [ 111 2049 4000 4001 4002 9091 20048 51413 ];
-        networking.firewall.allowedUDPPorts = [ 111 2049 4000 4001 4002 9091 20048 51413 ];
 
         services.xserver.enable = true;
         services.resolved.enable = true;
@@ -119,6 +91,6 @@
         system.stateVersion = "24.11";
       })
     ];
-    specialArgs = { inherit inputs; };
+    specialArgs = {inherit inputs;};
   };
 }
