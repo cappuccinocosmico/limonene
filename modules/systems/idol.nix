63bbc2746d5064b9574281227d840225450cadca
Nicole Venner <nvenner@protonmail.ch>
2026-08-03 22:05:25 -0600

    idol is a framework laptop

--- modules/systems/idol.nix
+++ modules/systems/idol.nix
@@ -13,10 +13,11 @@
       inputs.self.modules.nixos.sway
       inputs.self.modules.nixos.gaming
       inputs.self.modules.nixos.display-greetd
-      inputs.hardware.nixosModules.common-cpu-amd
-      inputs.hardware.nixosModules.common-cpu-amd-pstate
-      inputs.hardware.nixosModules.common-gpu-amd
-      inputs.hardware.nixosModules.common-pc-ssd
+      inputs.hardware.nixosModules.framework-16-amd-ai-300-series
+      # inputs.hardware.nixosModules.common-cpu-amd
+      # inputs.hardware.nixosModules.common-cpu-amd-pstate
+      # inputs.hardware.nixosModules.common-gpu-amd
+      # inputs.hardware.nixosModules.common-pc-ssd
       ../../hardware/idol.nix
       {
         limonene.machineType = "desktop";
@@ -25,13 +26,12 @@
 
         home-manager.users.nicole.imports = [inputs.self.modules.homeManager.nicole-desktop];
 
-        networking.hostName = "vermissian";
+        networking.hostName = "idol";
 
         boot.loader.systemd-boot.enable = true;
         boot.loader.efi.canTouchEfiVariables = true;
         boot.initrd.luks.devices."luks-1b5555a7-d2e4-4cf2-9654-f19eb0dfc349".device = "/dev/disk/by-uuid/1b5555a7-d2e4-4cf2-9654-f19eb0dfc349";
         boot.initrd.luks.devices."luks-099e44df-0372-4808-bf9a-74f2dba56f71".device = "/dev/disk/by-uuid/099e44df-0372-4808-bf9a-74f2dba56f71";
-
 
         i18n.defaultLocale = "en_US.UTF-8";
         i18n.extraLocaleSettings = {
